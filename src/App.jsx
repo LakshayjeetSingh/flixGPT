f58e2c6e19cbe0f890786e57d41e2b368b459de0
Lakshayjeet Singh <lakshaysingh9212@gmail.com>
2025-08-15 13:07:48 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className="flex h-screen items-center justify-center bg-neutral-800 text-neutral-100">
+      <h1>flixGPT</h1>{" "}
+    </main>
+  );
+}
+
+export default App;
